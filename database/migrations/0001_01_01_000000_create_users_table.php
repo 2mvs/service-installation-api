521b931a215d3495a9ea383f9dd25c24c51271d0
Theonis97 <boussougoumvoumbitheonisjozhy@gmail.com>
2025-08-18 00:28:08 +0100

    new commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('Telephone')->nullable();
+            $table->text('Adress')->nullable();
+            $table->enum('role', ['admin', 'client', 'installateur']);
             $table->rememberToken();
             $table->timestamps();
         });
